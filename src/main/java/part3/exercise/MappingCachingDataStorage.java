3de3e41e51ae7628a0b8ef2fd135ff0526dac083
Artem Lodygin <aldaer@yandex.ru>
2016-10-15 18:42:53 +0300

    Added Mapping and Pair data storages (without 2nd level caching)

--- src/main/java/part3/exercise/MappingCachingDataStorage.java
+++ src/main/java/part3/exercise/MappingCachingDataStorage.java
@@ -7,16 +7,21 @@ import java.util.function.Function;
 
 public class MappingCachingDataStorage<K, K1, T1, T> implements CachingDataStorage<K, T> {
 
+    private final CachingDataStorage<K1, T1> storage;
+    private final Function<K, K1> mapKey;
+    private final BiFunction<K, T1, T> mapValue;
+
     public MappingCachingDataStorage(CachingDataStorage<K1, T1> storage,
                                      Function<K, K1> mapKey,
                                      BiFunction<K, T1, T> mapValue) {
-        // TODO
-        throw new UnsupportedOperationException();
+        this.storage = storage;
+        this.mapKey = mapKey;
+        this.mapValue = mapValue;
     }
 
     @Override
     public OutdatableResult<T> getOutdatable(K key) {
-        // TODO
-        throw new UnsupportedOperationException();
+        final OutdatableResult<T1> response = storage.getOutdatable(mapKey.apply(key));
+        return new OutdatableResult<>(response.getResult().thenApply(t1 -> mapValue.apply(key, t1)), response.getOutdated());
     }
 }
