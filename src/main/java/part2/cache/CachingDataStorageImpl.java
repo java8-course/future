b5a186e4065917a1babc41a45aa4bc31747155d2
Alexandr Zykov <zikovam43@gmail.com>
2017-01-19 16:33:05 +0300

    done hometask2

--- src/main/java/part2/cache/CachingDataStorageImpl.java
+++ src/main/java/part2/cache/CachingDataStorageImpl.java
@@ -17,27 +17,51 @@ public class CachingDataStorageImpl<T> implements CachingDataStorage<String, T> 
                 private final ThreadFactory threadFactory = Executors.defaultThreadFactory();
 
                 @Override
-                public Thread newThread(Runnable r) {
+                public Thread newThread (Runnable r) {
                     final Thread thread = threadFactory.newThread(r);
                     thread.setDaemon(true);
                     return thread;
                 }
             });
 
-    public CachingDataStorageImpl(DataStorage<String, T> db, int timeout, TimeUnit timeoutUnits) {
+    public CachingDataStorageImpl (DataStorage<String, T> db, int timeout, TimeUnit timeoutUnits) {
         this.db = db;
         this.timeout = timeout;
         this.timeoutUnits = timeoutUnits;
     }
 
     @Override
-    public OutdatableResult<T> getOutdatable(String key) {
+    public OutdatableResult<T> getOutdatable (String key) {
         // TODO implement
         // TODO use ScheduledExecutorService to remove outdated result from cache - see SlowCompletableFutureDb implementation
         // TODO complete OutdatableResult::outdated after removing outdated result from cache
         // TODO don't use obtrudeException on result - just don't
         // TODO use remove(Object key, Object value) to remove target value
         // TODO Start timeout after receiving result in CompletableFuture, not after receiving CompletableFuture itself
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+        CompletableFuture<T> result = new CompletableFuture<>();
+        CompletableFuture<Void> outdated = new CompletableFuture<>();
+        OutdatableResult<T> outdatableResult = new OutdatableResult<>(result, outdated);
+        OutdatableResult<T> cachedResult = cache.putIfAbsent(key, outdatableResult);
+
+        if (cachedResult != null) {
+            return cachedResult;
+        }
+
+        db.get(key)
+                .whenComplete((t, throwable) -> {
+                    if (throwable != null) {
+                        result.completeExceptionally(throwable);
+                    } else {
+                        result.complete(t);
+                    }
+                    scheduledExecutorService.schedule(() -> {
+                                cache.remove(key, outdatableResult);
+                                outdated.complete(null);
+                            },
+                            timeout,
+                            timeoutUnits);
+                });
+        return  outdatableResult;
     }
 }
