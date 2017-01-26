928c6878b1cad932826a84a61ee90583f7affdc9
Max Elkin <maxim-el@mail.ru>
2017-01-27 00:59:03 +0300

    Hometask #3 completed

--- src/main/java/part2/cache/TypedEmployeeCachedStorage.java
+++ src/main/java/part2/cache/TypedEmployeeCachedStorage.java
@@ -4,6 +4,11 @@ import data.typed.Employee;
 import data.typed.Employer;
 import data.typed.Position;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
 public class TypedEmployeeCachedStorage implements CachingDataStorage<String, data.typed.Employee> {
 
     private final CachingDataStorage<String, data.Employee> employeeStorage;
@@ -19,8 +24,48 @@ public class TypedEmployeeCachedStorage implements CachingDataStorage<String, da
     }
 
     @Override
-    public OutdatableResult<Employee> getOutdatable(String key) {
+    public OutdatableResult<data.typed.Employee> getOutdatable(String key) {
         // TODO note that you don't know timeouts for different storage. And timeouts can be different.
-        throw new UnsupportedOperationException();
+        final OutdatableResult<data.Employee> notTypedOutdatable = employeeStorage.getOutdatable(key);
+        final CompletableFuture<Employee> typedEmployeeCf = new CompletableFuture<>();
+        final CompletableFuture<Void> outdatedCf = new CompletableFuture<>();
+
+        notTypedOutdatable.getResult().whenComplete((emp, ex) -> {
+            if (ex != null) {
+                typedEmployeeCf.completeExceptionally(ex);
+                return;
+            }
+
+            final List<CompletableFuture<Void>> outdatedSignals = new ArrayList<>();
+            outdatedSignals.add(notTypedOutdatable.getOutdated());
+            final List<CompletableFuture<data.typed.JobHistoryEntry>> jobHistoryCf = emp.getJobHistory().stream()
+                    .map(jhe -> {
+                        final OutdatableResult<Position> positionResult = positionStorage.getOutdatable(jhe.getPosition());
+                        outdatedSignals.add(positionResult.getOutdated());
+                        final OutdatableResult<Employer> employerResult = employerStorage.getOutdatable(jhe.getEmployer());
+                        outdatedSignals.add(employerResult.getOutdated());
+                        return positionResult.getResult().thenCombine(employerResult.getResult(),
+                                (position, employer) -> new data.typed.JobHistoryEntry(position, employer, jhe.getDuration()));
+                    })
+                    .collect(Collectors.toList());
+            CompletableFuture.anyOf(outdatedSignals.toArray(new CompletableFuture[0])).whenComplete((v, t) -> {
+                if (t != null) {
+                    outdatedCf.completeExceptionally(t);
+                } else {
+                    outdatedCf.complete((Void) v);
+                }
+            });
+            CompletableFuture.allOf(jobHistoryCf.toArray(new CompletableFuture[0])).whenComplete((v, t) -> {
+                if (t != null) {
+                    typedEmployeeCf.completeExceptionally(t);
+                    return;
+                }
+                final List<data.typed.JobHistoryEntry> typedJobHistory = jobHistoryCf.stream()
+                        .map(CompletableFuture::join)
+                        .collect(Collectors.toList());
+                typedEmployeeCf.complete(new Employee(emp.getPerson(), typedJobHistory));
+            });
+        });
+        return new OutdatableResult<>(typedEmployeeCf, outdatedCf);
     }
 }
--- src/test/java/part2/cache/TypedEmployeeCachedStorageTest.java
+++ src/test/java/part2/cache/TypedEmployeeCachedStorageTest.java
@@ -1,19 +1,25 @@
 package part2.cache;
 
 import data.Employee;
+import data.Generator;
+import data.JobHistoryEntry;
+import data.Person;
 import data.typed.Employer;
 import data.typed.Position;
 import db.SlowCompletableFutureDb;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runners.JUnit4;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toMap;
 
@@ -49,7 +55,7 @@ public class TypedEmployeeCachedStorageTest {
     }
 
     @Test
-    public void expiration() {
+    public void expiration() throws InterruptedException {
         final CachingDataStorageImpl<Employee> employeeCache =
                 new CachingDataStorageImpl<>(employeeDb, 1, TimeUnit.SECONDS);
 
@@ -63,5 +69,33 @@ public class TypedEmployeeCachedStorageTest {
                 new TypedEmployeeCachedStorage(employeeCache, positionCache, employerCache);
 
         // TODO check than cache gets outdated with the firs outdated inner cache
+        final Map<String, data.Employee> employeeMap =
+                Generator.generateEmployeeList(10).stream()
+                        .collect(Collectors.toMap((data.Employee e) -> e.getPerson().toString(),Function.identity()));
+        employeeDb.setValues(employeeMap);
+
+        final Iterator<String> untypedKeys = employeeMap.keySet().iterator();
+        final String key = untypedKeys.next();
+        final Employee untypedEmployee = employeeMap.get(key);
+        final data.typed.Employee typedEmployee = typedCache.get(key).join();
+        Assert.assertEquals(untypedEmployee.getPerson(),typedEmployee.getPerson());
+        final Iterator<data.typed.JobHistoryEntry> typedJobHistory = typedEmployee.getJobHistoryEntries().iterator();
+        untypedEmployee.getJobHistory().stream()
+                .forEach(jhe -> {
+                    data.typed.JobHistoryEntry typedEntry = typedJobHistory.next();
+                    Assert.assertEquals(jhe.getDuration(),typedEntry.getDuration());
+                    Assert.assertEquals(jhe.getEmployer(),typedEntry.getEmployer().toString());
+                    Assert.assertEquals(jhe.getPosition(),typedEntry.getPosition().toString());
+                });
+        String key2 = untypedKeys.next();
+        while (employeeMap.get(key2).getJobHistory().isEmpty()){
+            key2 = untypedKeys.next();
+        }
+        final CompletableFuture<Void> outdated = typedCache.getOutdatable(key2).getOutdated();
+        Assert.assertFalse(outdated.isDone());
+        Thread.sleep(50);
+        Assert.assertFalse(outdated.isDone());
+        Thread.sleep(60);
+        Assert.assertTrue(outdated.isDone());
     }
 }
