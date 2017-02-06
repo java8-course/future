3e53b8e986806bc2a0de8ea95ea009383e98000f
Alexandr Zykov <zikovam43@gmail.com>
2017-02-06 10:24:39 +0300

    done hw3

--- src/main/java/part3/exercise/ComposeCachingDataStorage.java
+++ src/main/java/part3/exercise/ComposeCachingDataStorage.java
@@ -2,20 +2,49 @@ package part3.exercise;
 
 import part2.cache.CachingDataStorage;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 public class ComposeCachingDataStorage<K1, T1, K2, T2> implements CachingDataStorage<K1, T2> {
 
-    public ComposeCachingDataStorage(CachingDataStorage<K1, T1> storage1,
-                                     CachingDataStorage<K2, T2> storage2,
-                                     Function<T1, K2> mapping) {
-        // TODO
-        throw new UnsupportedOperationException();
+    private CachingDataStorage<K1, T1> storage1;
+    private CachingDataStorage<K2, T2> storage2;
+    private Function<T1, K2> mapping;
+
+    public ComposeCachingDataStorage (CachingDataStorage<K1, T1> storage1,
+                                      CachingDataStorage<K2, T2> storage2,
+                                      Function<T1, K2> mapping) {
+        this.storage1 = storage1;
+        this.storage2 = storage2;
+        this.mapping = mapping;
     }
 
     @Override
-    public OutdatableResult<T2> getOutdatable(K1 key) {
-        // TODO
-        throw new UnsupportedOperationException();
+    public OutdatableResult<T2> getOutdatable (K1 key) {
+
+        final CompletableFuture<Void> outdated = new CompletableFuture<>();
+        final OutdatableResult<T1> outdatableT1 = storage1.getOutdatable(key);
+        final CompletableFuture<T1> resultT1 = outdatableT1.getResult();
+        final CompletableFuture<K2> resultK2 = resultT1.thenApply(mapping);
+
+        final CompletableFuture<OutdatableResult<T2>> outdatableResultCompletableFuture = resultK2.thenApply(x -> storage2.getOutdatable(x));
+        final CompletableFuture<T2> cfT2 = outdatableResultCompletableFuture
+                .thenCompose(OutdatableResult::getResult);
+
+        outdatableResultCompletableFuture.thenCompose(OutdatableResult::getOutdated)
+                .whenComplete(whenComplete(outdated));
+
+        return new OutdatableResult<>(cfT2, outdated);
+    }
+
+    private BiConsumer<Void, Throwable> whenComplete (CompletableFuture<Void> outdated) {
+        return (value, exception) -> {
+            if (exception != null) {
+                outdated.completeExceptionally(exception);
+            } else {
+                outdated.complete(null);
+            }
+        };
     }
 }
--- src/main/java/part3/exercise/ListCachingDataStorage.java
+++ src/main/java/part3/exercise/ListCachingDataStorage.java
@@ -3,17 +3,39 @@ package part3.exercise;
 import part2.cache.CachingDataStorage;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.stream.Collectors.toList;
 
 public class ListCachingDataStorage<K, T> implements CachingDataStorage<List<K>, List<T>> {
 
-    public ListCachingDataStorage(CachingDataStorage<K, T> storage) {
-        // TODO
-        throw new UnsupportedOperationException();
+    CachingDataStorage<K, T> storage;
+
+    public ListCachingDataStorage (CachingDataStorage<K, T> storage) {
+        this.storage = storage;
     }
 
     @Override
-    public OutdatableResult<List<T>> getOutdatable(List<K> key) {
-        // TODO
-        throw new UnsupportedOperationException();
+    public OutdatableResult<List<T>> getOutdatable (List<K> key) {
+        final List<OutdatableResult<T>> outdatableList = key.stream()
+                .map(x -> storage.getOutdatable(x))
+                .collect(toList());
+
+        final CompletableFuture<T>[] rcf = outdatableList.stream()
+                .map(OutdatableResult::getResult)
+                .toArray(i -> (CompletableFuture<T>[]) new CompletableFuture[i]);
+        final CompletableFuture<List<T>> lcf = CompletableFuture.allOf(rcf)
+                .thenApply((v) -> outdatableList.stream()
+                        .map(OutdatableResult::getResult)
+                        .map(CompletableFuture::join)
+                        .collect(toList()));
+
+        final CompletableFuture<Void>[] ocf = outdatableList.stream()
+                .map(OutdatableResult::getOutdated)
+                .toArray(i -> (CompletableFuture<Void>[]) new CompletableFuture[i]);
+        final CompletableFuture<Void> outdated = CompletableFuture.anyOf(ocf)
+                .thenApply(x -> null);
+
+        return new OutdatableResult<>(lcf, outdated);
     }
 }
