31b87f0c1d04a6795dc47f7f654ec06461de5397
MortyMerr <anton.nazarof@mail.ru>
2017-07-28 16:24:07 +0300

    cashsingdatastorage

--- src/main/java/part3/exercise/ListCachingDataStorage.java
+++ src/main/java/part3/exercise/ListCachingDataStorage.java
@@ -3,17 +3,44 @@ package part3.exercise;
 import part2.cache.CachingDataStorage;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 public class ListCachingDataStorage<K, T> implements CachingDataStorage<List<K>, List<T>> {
+    private final CachingDataStorage<K, T> storage;
 
     public ListCachingDataStorage(CachingDataStorage<K, T> storage) {
-        // TODO
-        throw new UnsupportedOperationException();
+        this.storage = storage;
+    }
+
+    private <T> T getOrNull(Future<T> f) {
+        try {
+            return f.get();
+        } catch (InterruptedException | ExecutionException e1) {
+            e1.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public OutdatableResult<List<T>> getOutdatable(List<K> key) {
-        // TODO
-        throw new UnsupportedOperationException();
+        final List<OutdatableResult<T>> collect = key.stream()
+                .map(storage::getOutdatable)
+                .collect(Collectors.toList());
+
+        final CompletableFuture<Void> voidCompletableFuture = CompletableFuture.allOf(collect.stream()
+                .map(OutdatableResult::getResult)
+                .toArray(CompletableFuture[]::new));
+
+        return new OutdatableResult<>(voidCompletableFuture.thenApply(v -> collect.stream()
+                .map(OutdatableResult::getResult)
+                .map(this::getOrNull)
+                .collect(Collectors.toList())),
+                CompletableFuture.anyOf(collect.stream()
+                        .map(OutdatableResult::getOutdated)
+                        .toArray(CompletableFuture[]::new))
+                        .thenApply(v -> null));
     }
 }
--- src/main/java/part3/exercise/MappingCachingDataStorage.java
+++ src/main/java/part3/exercise/MappingCachingDataStorage.java
@@ -6,17 +6,23 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class MappingCachingDataStorage<K, K1, T1, T> implements CachingDataStorage<K, T> {
+    private final CachingDataStorage<K1, T1> storage;
+    private final Function<K, K1> mapKey;
+    private final BiFunction<K, T1, T> mapValue;
 
     public MappingCachingDataStorage(CachingDataStorage<K1, T1> storage,
                                      Function<K, K1> mapKey,
                                      BiFunction<K, T1, T> mapValue) {
-        // TODO
-        throw new UnsupportedOperationException();
+        this.storage = storage;
+        this.mapKey = mapKey;
+        this.mapValue = mapValue;
     }
 
     @Override
     public OutdatableResult<T> getOutdatable(K key) {
-        // TODO
-        throw new UnsupportedOperationException();
+        final OutdatableResult<T1> outdatable = storage.getOutdatable(mapKey.apply(key));
+
+        return new OutdatableResult<>(outdatable.getResult().thenApply(v -> mapValue.apply(key, v)),
+                outdatable.getOutdated());
     }
 }
